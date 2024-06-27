be76deae47a199fc43eb15c6e329da8d27dd3a05
ODINKS <nicholas.okeke87@gmail.com>
2024-06-27 21:28:23 +0100

    fix: make some structural arrangement

--- components/landing-page/Header.tsx
+++ components/landing-page/Header.tsx
@@ -9,8 +9,8 @@ import { HeaderProps } from "./utils/types";
 function Header({ onClick }: HeaderProps) {
   return (
     <header className="fixed inset-x-0 top-0 z-10 flex justify-around text-xs md:text-sm lg:text-base px-2 md:px-0 py-5 bg-[#F5F4F6] overflow-hidden ">
-      <div className="self-center flex gap-20 md:hidden">
-        <button className="w-fit h-fit" onClick={onClick}>
+      <div className="self-center flex gap-20">
+        <button className="w-fit h-fit md:hidden" onClick={onClick}>
           <Icons.menu />
         </button>
         <Logo className="self-center" />
@@ -20,7 +20,7 @@ function Header({ onClick }: HeaderProps) {
           {navData.map((nav, index) => {
             return (
               <li key={index}>
-                <Link href={nav.href}>{nav.name}</Link>
+                <Link href={nav.href} className="font-medium">{nav.name}</Link>
               </li>
             );
           })}
--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <section className="min-h-[87vh] bg-[url('/hero_img.png')] bg-cover md:bg-contain bg-center flex justify-center py-20 px-2">
-      <h1 className="font-bold text-3xl lg:text-6xl flex flex-col gap-3">
+      <h1 className="font-bold text-3xl lg:text-6xl flex flex-col t text-center lg:text-start gap-3 max-w-[40rem]">
         Let&apos;s create something great together.
       </h1>
     </section>
